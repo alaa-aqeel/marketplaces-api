aa6c79a80d75a285a02f249c569330a11a2aa8a0
Alaa Aqeel <alaa.21.iraq@gmail.com>
2025-09-17 18:33:51 +0300

    modified:   tests/Unit/CircuitBreakerTest.php

--- tests/Unit/CircuitBreakerTest.php
+++ tests/Unit/CircuitBreakerTest.php
@@ -2,12 +2,41 @@
 
 namespace Tests\Unit;
 
+use App\Helper\CircuitBreaker;
 use App\Marketplaces\AlibabaMarketplace;
 use App\Marketplaces\AliExpressMarketplace;
 use App\Services\MarketplaceService;
+use Exception;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class CircuitBreakerTest extends TestCase
 {
+
+    function circuitBreaker($name, $cacheKey)
+    {
+        return new CircuitBreaker($name, $cacheKey);
+    }
+
+    function test_success_request()
+    {
+        $resp = $this->circuitBreaker("name", "cache_key")->call(function() {
+            return Http::acceptJson()->get("https://dummyjson.com/products/search");
+        }, resetTimeout: 60);
+        $this->assertTrue($resp->successful());
+    }
+
+    function test_with_exception()
+    {
+        $resp = $this->circuitBreaker("name", "cache_key")->call(function() {
+            return Http::acceptJson()->get("https://dummyjson.com/products/search");
+        }, resetTimeout: 60);
+        $this->assertTrue($resp->successful());
+
+        $resp = $this->circuitBreaker("name", "cache_key")->call(function() { // get from cache
+            throw new Exception("");
+        }, resetTimeout: 60);
+        $this->assertTrue($resp->successful());
+    }
 
 }
